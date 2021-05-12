c1e2ffef8f8b7e7b82de6d4c740684e8fd6c9f81
sanjiu <1053835782@qq.com>
2021-05-13 01:37:13 +0800

    暂存

--- 手写/test.js
+++ 手写/test.js
@@ -1,7 +1,12 @@
-// Node.js 中有一个queryString模块，可以实现将 urlStr 主机地址后面的参数转化为对象。
-let urlStr = 'http://www.inode.club?name=koala&study=js&study=node';
-
-
-function queryString()
-
-// https://juejin.cn/post/6844904073351675912
+let get = new Promise((resolve, reject) => {
+    resolve('https://en.wikipedia.org/wiki/Robert_Cecil_Martin')
+})
+    .then((response) => {
+        return console.log(response);
+    })
+    .then(() => {
+        console.log('File written');
+    })
+    .catch((err) => {
+        console.error(err);
+    });
