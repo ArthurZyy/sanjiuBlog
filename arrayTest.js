01260c16f6dfa63b2829d73dee3da508f5f46903
sanjiu <1053835782@qq.com>
2021-05-23 11:16:57 +0800

    暂存

---
+++ arrayTest.js
@@ -0,0 +1,34 @@
+function checkArray(array) {
+    if (!array) return
+}
+function swap(array, left, right) {
+    let rightValue = array[right]
+    array[right] = array[left]
+    array[left] = rightValue
+}
+
+let array = [9, 1, 5, 4]
+
+function bubble(array) {
+    checkArray(array)
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) swap(array, j, j+1)
+
+        }
+    }
+    return array
+}
+
+console.log(bubble(array))
+
+function insertion (array){
+    checkArray(array)
+    for (let i = 0; i < array.length; i++) {
+        for (let j = i-1; j > 0; j++) {
+            const element = array[j];
+            
+        }
+        
+    }
+}
